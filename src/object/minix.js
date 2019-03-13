7982d602671d85c8cff21ab10e7d33e61442ac90
wangyimei <nina.wangyimei@gmail.com>
2019-03-13 22:39:49 +0800

    add minix.js

--- src/object/minix.js
+++ src/object/minix.js
@@ -1,7 +1,20 @@
 /**
  * 组合继承
- * @param {*} o 
+ * ------------------------------
+ * 组合继承使用构造函数模式定义实例属性，
+ * 原型模式用于定义方法和共享属性
+ * ------------------------------
  */
-function Parent() {
-    
+function Person(name, age, job) {
+    this.name = name
+    this.age = age
+    this.job = job
+    this.friends = ['nina', 'bob']
 }
+Person.prototype = {
+    constructor: Person,
+    sayName: function () {
+        console.log(this.name)
+    }
+}
+module.exports = Person
