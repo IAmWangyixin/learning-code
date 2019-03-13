63ec9505bdfebaaf11a590d5e8415db6e3fee07a
wangyimei <nina.wangyimei@gmail.com>
2019-03-13 22:46:31 +0800

    fix minix test

--- test/minix.js
+++ test/minix.js
@@ -2,15 +2,15 @@ var assert = require('assert')
 
 describe('ES5 组合继承：', function() {
     var createPerson = require('../src/object/minix.js')
-    var person1 = createPerson('test1', '18', 'sudents')
-    var person2 = createPerson('nina', '2*', 'web developer')
+    var person1 = new createPerson('test1', '18', 'sudents')
+    var person2 = new createPerson('nina', '2*', 'web developer')
 
     it('构造方法让实例拥有自己的属性', function() {
-        person1.name = test2
+        person1.name = 'test2'
         person1.friends.push('neinei')
         assert.deepStrictEqual(person1.name, 'test2')
         assert.deepStrictEqual(person2.name, 'nina')
-        assert.notStrictEqual(person2.friends, person2.friends)
+        assert.notStrictEqual(person1.friends, person2.friends)
     })
 
     it('原型方法让实例间拥有共同的方法', function(){
