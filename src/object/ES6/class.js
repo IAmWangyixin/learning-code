0b47f977b9f0b1b77fa25232d9da335aca55e51e
wangyimei <nina.wangyimei@gmail.com>
2019-05-19 12:16:00 +0800

    add class extend notes

--- src/object/ES6/class.js
+++ src/object/ES6/class.js
@@ -20,6 +20,7 @@ console.log(Object.keys(Point.prototype)); //[]
 let instance = new Point();
 instance.prop = 123; // setter:123
 instance.prop; // getter
+
 const descriptor = Object.getOwnPropertyDescriptor(Point.prototype, 'prop');
 console.log('get' in descriptor);
 console.log('set' in descriptor);
@@ -35,34 +36,60 @@ console.log(new Foo() instanceof Foo); // false
 
 class Foo1 {
     static bar() {
-      this.baz();
+        this.baz();
     }
     static baz() {
-      console.log('hello');
+        console.log('hello');
     }
     baz() {
-      console.log('world');
+        console.log('world');
     }
-  }
-  
+}
+
 Foo1.bar();// hello
 
 class Shape {
     constructor() {
-      if (new.target === Shape) {
-        throw new Error('本类不能实例化');
-      } else {
-        console.log('success')
-      }
+        if (new.target === Shape) {
+            throw new Error('本类不能实例化');
+        } else {
+            console.log('success')
+        }
     }
-  }
-  
-  class Rectangle extends Shape {
+}
+
+class Rectangle extends Shape {
     constructor(length, width) {
-      super();
-      // ...
+        super();
+        // ...
     }
-  }
-  
+}
+
 //   var x = new Shape();  // 报错
-  var y = new Rectangle(3, 4);  // 正确
+var y = new Rectangle(3, 4);  // 正确
+
+// 继承
+class A {
+    constructor() {
+        this.x = 1;
+        this.test = 'test';
+    }
+    static print() {
+        console.log(this.x);
+    }
+}
+
+class B extends A {
+    constructor() {
+        super();
+        this.x = 2;
+    }
+    static m() {
+        console.log(super.test);
+        super.print();
+    }
+}
+B.x = 3;
+B.m();
+  //  undefined
+  // 3
